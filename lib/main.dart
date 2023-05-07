eff025305f4c866ff698dc8071fabc6737d293e8
Thanh Phong <pttp15122002@gmail.com>
2023-05-07 13:39:58 +0700

    push

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'home.dart';
 import 'MaterialColor.dart';
 
-import 'package:orm/logger.dart';
-
-import './generated/prisma_client.dart';
-// Color myColor = Color(0xFFffb3c1);
-
-final prisma = PrismaClient(
-  stdout: Event.values, // print all events to the console
-  datasources: const Datasources(
-    db: 'mongodb+srv://pttp15122002:Uh7HUtXGGcRVdwI3@notexpert.gdoldu9.mongodb.net/test',
-  ),
-);
+//
+// import 'package:orm/logger.dart';
+//
+// import './generated/prisma_client.dart';
+// // Color myColor = Color(0xFFffb3c1);
+//
+// final prisma = PrismaClient(
+//   stdout: Event.values, // print all events to the console
+//   datasources: const Datasources(
+//     db: 'mongodb+srv://pttp15122002:Uh7HUtXGGcRVdwI3@notexpert.gdoldu9.mongodb.net/test',
+//   ),
+// );
 void main() async {
-  try {
-    final user = await prisma.user.create(
-      data: const UserCreateInput(
-          username: "thanhphong", password: "15122002", name: "Thanh Phong"),
-    );
-
-    final note = await prisma.note.create(
-        data: const NoteCreateInput(title: "New Note", content: "Test Prisma"));
-  } finally {
-    await prisma.$disconnect();
-  }
-  return runApp(MyApp());
+  // try {
+  //   final user = await prisma.user.create(
+  //     data: const UserCreateInput(
+  //         username: "thanhphong", password: "15122002", name: "Thanh Phong"),
+  //   );
+  //
+  //   final note = await prisma.note.create(
+  //       data: const NoteCreateInput(title: "New Note", content: "Test Prisma"));
+  // } finally {
+  //   await prisma.$disconnect();
+  // }
+  // return
+  runApp(MyApp());
 }
 
 enum _MenuValues {
@@ -57,6 +60,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var noteList = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -65,44 +69,74 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Container(
         alignment: Alignment.topRight,
-        child: PopupMenuButton(
-          itemBuilder: (BuildContext context) => [
-            PopupMenuItem(child: Text('Share App'), value: _MenuValues.Share),
-            PopupMenuItem(child: Text('Security'), value: _MenuValues.Security),
-            PopupMenuItem(
-                child: Text('Privacy Policy'), value: _MenuValues.Privacy),
-            PopupMenuItem(child: Text('Favorite'), value: _MenuValues.Favorite),
-            PopupMenuItem(child: Text('Settings'), value: _MenuValues.Settings),
-          ],
-          onSelected: (value) {
-            switch (value) {
-              case _MenuValues.Favorite:
-                Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (c) => Favorite()));
-                break;
-              case _MenuValues.Share:
-                Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (c) => Share()));
-                break;
-              case _MenuValues.Privacy:
-                Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (c) => Privacy()));
-                break;
-              case _MenuValues.Security:
-                Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (c) => Security()));
-                break;
-              case _MenuValues.Settings:
-                Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (c) => Settings()));
-                break;
-            }
-          },
+        child: ListView(
+          children: noteList
+              .map((e) => ListTile(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (ctx) => EditNote(note: e),
+                          ));
+                    },
+                    leading: Icon(Icons.list),
+                    title: Text(e['title'] as String),
+                    subtitle: Text(e['message'] as String),
+                    trailing: IconButton(
+                        onPressed: () {}, icon: Icon(Icons.more_vert)),
+                  ))
+              .toList(),
         ),
+        // child: PopupMenuButton(
+        //   itemBuilder: (BuildContext context) => [
+        //     PopupMenuItem(child: Text('Share App'), value: _MenuValues.Share),
+        //     PopupMenuItem(child: Text('Security'), value: _MenuValues.Security),
+        //     PopupMenuItem(
+        //         child: Text('Privacy Policy'), value: _MenuValues.Privacy),
+        //     PopupMenuItem(child: Text('Favorite'), value: _MenuValues.Favorite),
+        //     PopupMenuItem(child: Text('Settings'), value: _MenuValues.Settings),
+        //   ],
+        //   onSelected: (value) {
+        //     switch (value) {
+        //       case _MenuValues.Favorite:
+        //         Navigator.of(context)
+        //             .push(MaterialPageRoute(builder: (c) => Favorite()));
+        //         break;
+        //       case _MenuValues.Share:
+        //         Navigator.of(context)
+        //             .push(MaterialPageRoute(builder: (c) => Share()));
+        //         break;
+        //       case _MenuValues.Privacy:
+        //         Navigator.of(context)
+        //             .push(MaterialPageRoute(builder: (c) => Privacy()));
+        //         break;
+        //       case _MenuValues.Security:
+        //         Navigator.of(context)
+        //             .push(MaterialPageRoute(builder: (c) => Security()));
+        //         break;
+        //       case _MenuValues.Settings:
+        //         Navigator.of(context)
+        //             .push(MaterialPageRoute(builder: (c) => Settings()));
+        //         break;
+        //     }
+        //   },
+        // ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () async {
+          var note = await Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (ct) => EditNote(),
+            ),
+          );
+          if (note != null) {
+            setState(() {
+              noteList.add(note);
+            });
+          }
+        },
         child: Icon(Icons.add),
         backgroundColor: Color(0xFFffb3c1),
         foregroundColor: Colors.white,
@@ -185,6 +219,94 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
       ),
+    );
+  }
+}
+
+class EditNote extends StatefulWidget {
+  var note;
+  EditNote({this.note, super.key});
+  @override
+  State<EditNote> createState() => _EditNoteState();
+}
+
+class Note {}
+
+class _EditNoteState extends State<EditNote> {
+  var _key = GlobalKey<FormState>();
+  var focus = FocusNode();
+  String title = '';
+  String message = '';
+
+  void _saveForm() async {
+    final prefs = await SharedPreferences.getInstance();
+    if (_key.currentState?.validate() ?? false) {
+      _key.currentState?.save();
+      Navigator.pop(context, {'title': title, 'message': message});
+    } else {
+      focus.requestFocus();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:
+          AppBar(title: Text(widget.note != null ? 'Edit Note' : 'New Note')),
+      body: Form(
+          key: _key,
+          child: Container(
+            padding: EdgeInsets.all(16),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextFormField(
+                    initialValue:
+                        widget.note != null ? widget.note['title'] : ' ',
+                    focusNode: focus,
+                    onSaved: (v) {
+                      title = v ?? '';
+                    },
+                    validator: (v) {
+                      if (v == null || v.isEmpty) return 'Please enter title';
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), labelText: 'Title'),
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  TextFormField(
+                    initialValue:
+                        widget.note != null ? widget.note['message'] : ' ',
+                    onSaved: (v) {
+                      message = v ?? '';
+                    },
+                    validator: (v) {
+                      if (v == null || v.isEmpty) return 'Please enter content';
+                      if (v.length < 10) return 'Note content is to short';
+                      return null;
+                    },
+                    maxLines: 10,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), labelText: 'Content'),
+                  ),
+                  SizedBox(
+                    height: 120,
+                  ),
+                  ElevatedButton(
+                      onPressed: _saveForm,
+                      child: Text(
+                          widget.note != null ? 'Save Changes' : 'Create Note'),
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size.fromHeight(50),
+                      )),
+                ],
+              ),
+            ),
+          )),
     );
   }
 }
